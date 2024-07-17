3272190dde6bfc8e38ee2746b2153663c2a51253
seifalrahman <130805731+seifalrahman@users.noreply.github.com>
2024-07-18 00:26:59 +0300

    Update MoviesNews.js

--- MoviesNews/MoviesNews.js
+++ MoviesNews/MoviesNews.js
@@ -1,6 +1,62 @@
-let movies =document.getElementById("movies");
+let movies          =  document.getElementById("movies");
+let menuicon        =  document.getElementById("menuicon") ;
+let bigsidebar      =  document.getElementById("bsb") ;
+let smallsidebar    =  document.getElementById("ssb"); 
+/*left :43vh;*/
+let ishidden=1;
+let nowPlaying      =  document.getElementById("NP") ;
+let popular         =  document.getElementById("P");
+let topRated        =  document.getElementById("TopR");
+let trending        =  document.getElementById("trend");
+let upcoming        =  document.getElementById("coming");
+let contactUs       =  document.getElementById("ContactUs");
 
 
+nowPlaying.addEventListener("click",function(){
+    moviesGallery="";
+    request(nowPlayingMoviesEndpoint) ;
+})
+
+popular.addEventListener("click" ,function(){
+    moviesGallery="";
+    request(popularMoviesEndpoint) ;
+})
+
+topRated.addEventListener("click",function(){
+    moviesGallery="";
+    request(topRatedMoviesEndpoint) ;
+})
+trending.addEventListener("click",function(){
+    moviesGallery="";
+    request(trendingMoviesEndpoint) ;
+})
+upcoming.addEventListener("click",function(){
+    moviesGallery="";
+    request(upcomingMoviesEndpoint) ;
+})
+
+
+/* <div class="bigsidebartxt">
+                    <p id="NP">Now playing</p>
+                    <p id="P">Popular</p>
+                    <p id="TopR">Top Rated</p>
+                    <p id="trend">Trending</p>
+                    <p id="coming">Upcoming</p>
+                    <p id="ContactUs">Contact Us</p>
+</div> */
+
+menuicon.addEventListener("click" , function(){
+    if(ishidden){
+        smallsidebar.style.left="42vh";
+        bigsidebar.style.display="block";
+        ishidden=0;
+    }else{
+        smallsidebar.style.left="0vh";
+        bigsidebar.style.display="none";
+        ishidden=1;
+    }
+
+})
 const API_Key = "f2b59963881f67e8d8dbe04a25ebf9f6";
 const nowPlayingMoviesEndpoint =` https://api.themoviedb.org/3/movie/now_playing?api_key=${API_Key}`;
 const popularMoviesEndpoint = `https://api.themoviedb.org/3/movie/popular?api_key=${API_Key}`;
@@ -11,6 +67,8 @@ const topRatedMoviesEndpoint = `https://api.themoviedb.org/3/movie/top_rated?api
 let data;
 let dataresults ;
 let moviesGallery="";
+let imgids=[];
+let imgentities=[];
 
 function request(URL) {
     fetch(URL)
@@ -21,17 +79,52 @@ function request(URL) {
             return response.json();
         })
         .then(data => {
+            imgids=[];
+            pentities=[];
+            imgentities=[];
             console.log(data);
             data.results.forEach(movie => {
+                imgids.push(movie.id);
+
                 moviesGallery+=`
                 <div class="imgdiv">
-                    <img class="imagee" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                    <img class="imagee" id="${movie.id}" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                    <p id="p${movie.id}"></p>
                 </div>
                 `
             });
 
             movies.innerHTML=moviesGallery;
-            
+            for (let i =0 ; i<imgids.length ;i++ ){
+                imgentities.push(document.getElementById(`${imgids[i]}`));
+                pentities.push(document.getElementById("p"+imgids[i])) ;
+                
+                imgentities[imgentities.length - 1 ].addEventListener("mouseenter" ,function(){
+                    for(let j =0 ; j<(data.results).length;j++ ){
+                        if(data.results[j].id===imgids[i]){
+
+                            imgentities[j].src=`https://image.tmdb.org/t/p/w500${data.results[j].backdrop_path}` ;
+                            pentities[j].innerText=data.results[j].overview;
+                            pentities[j].style.color="rgba(255,255,255,0.7)";
+                        }
+
+                    }
+                })
+
+                imgentities[imgentities.length - 1 ].addEventListener("mouseleave" ,function(){
+                    for(let j =0 ; j<(data.results).length;j++ ){
+                        if(data.results[j].id===imgids[i]){
+                            pentities[j].innerText="";
+                            imgentities[j].src=`https://image.tmdb.org/t/p/w500${data.results[j].poster_path}` ;
+                        }
+
+                    }
+                })
+                
+
+
+
+            }
         })
         .catch(error => {
             console.error('Fetch error:', error);
@@ -39,3 +132,7 @@ function request(URL) {
 }
 
 request(nowPlayingMoviesEndpoint);
+
+
+
+
